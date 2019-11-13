d728981ef3f3ec62d19be9053d4215fe61ab9131
RicardoSaracnio <ricardo.saracino@053gc.onmicrosoft.com>
2019-11-13 14:15:40 -0500

    A few PDF Gens

---
+++ PDFGen/Controllers/ClownController.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using org.pdfclown.documents;
+using org.pdfclown.files;
+using org.pdfclown.documents.contents.composition;
+using org.pdfclown.documents.contents.fonts;
+using File = org.pdfclown.files.File;
+
+namespace PDFGen.Controllers
+{
+    [RoutePrefix("api/pdf")]
+    public class ClownController : ApiController
+    {
+        [Route("clown")]
+        public HttpResponseMessage Get()
+        {
+            // https://pdfclown.org/overview/features/#PDFCreation
+            
+            var file = new File();
+            
+// 1. Instantiate a new PDF document!
+            var document = file.Document;
+
+// 2. Add a page to the document!
+            var page = new Page(document); // Instantiates the page inside the document context.
+            
+            document.Pages.Add(page); // Puts the page in the pages collection (you may choose an arbitrary position).
+
+// 3. Create a content composer for the page!
+            var composer = new PrimitiveComposer(page);
+
+// 4. Add contents through the composer!
+            composer.SetFont(new StandardType1Font(document, StandardType1Font.FamilyEnum.Courier, true, false), 32);
+            composer.ShowText("Hello World!", new PointF(32, 48));
+            
+            composer.Flush();
+            
+            byte[] pdf;
+            
+            using (var oms = new MemoryStream())
+            {
+                using (org.pdfclown.bytes.IOutputStream os = new org.pdfclown.bytes.Stream(oms))
+                {
+                    file.Save(os, SerializationModeEnum.Incremental);
+                }
+
+                pdf = oms.ToArray();
+            }
+
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK) {Content = new ByteArrayContent(pdf)};
+            response.Content.Headers.ContentDisposition =
+                new ContentDispositionHeaderValue("inline") {FileName = "template.pdf"};
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            return response;
+        }
+    }
+}
---
+++ PDFGen/Controllers/SautinController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Hosting;
+using System.Web.Http;
+using SautinSoft.Document;
+
+namespace PDFGen.Controllers
+{
+    [RoutePrefix("api/pdf")]
+    public class SautinController : ApiController
+    {
+        [Route("sautin")]
+        public HttpResponseMessage Get()
+        {
+            // https://code.msdn.microsoft.com/windowsapps/Create-digitally-signed-1ab9e3c3?fbclid=IwAR3DgEhRbiCCozagd5BzYXoWoU1P_Gm5ng77aEvHdozH-QOSTNtoyJ_jH1M
+
+            var loadPath = HostingEnvironment.MapPath(@"~/App_Data/template.docx");
+
+            //DocumentCore.Serial = "put your serial here"; 
+            var document = DocumentCore.Load(loadPath);
+            
+            var options = new PdfSaveOptions()
+            {
+                DigitalSignature =
+                {
+                    // Path to the certificate (*.pfx). 
+                    CertificatePath = HostingEnvironment.MapPath(@"~/App_Data/mycert.pfx"),
+                    // Password of the certificate. 
+                    CertificatePassword = "test",
+                    // Additional information about the certificate. 
+                    Location = "World Wide Web",
+                    Reason = "Document.Net by SautiSoft",
+                    ContactInfo = "info@sautinsoft.com",
+                    // Placeholder where signature should be visualized. 
+                    // SignatureLine = signatureLine, 
+                    // Visual representation of digital signature. 
+                    //Signature = signature 
+                }
+            };
+            
+            byte[] pdf;
+
+            using (var msPdf = new MemoryStream())
+            {
+                document.Save(msPdf, options);
+                pdf = msPdf.ToArray();
+            }
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK) {Content = new ByteArrayContent(pdf)};
+            response.Content.Headers.ContentDisposition =
+                new ContentDispositionHeaderValue("attachment") {FileName = "template.pdf"};
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            return response;
+        }
+    }
+}
